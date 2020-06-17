0fd58e642084b20c70cfe95b087289a970b744ea
BlackDragon-B <59388636+BlackDragon-B@users.noreply.github.com>
2020-06-17 07:56:10 +0200

    Create uninstall.lua
    Added uninstall.lua for uninstalling dragonos less destructive.

---
+++ dragonos/programs/uninstall.lua
@@ -0,0 +1,12 @@
+-- Uninstall by BlackDragon#8528
+-- Less destructive way of removing the OS
+
+write("Are you sure u want to uninstall"..ver.."? Y/N ")
+local choice = read()
+if choice == Y or choice == y then
+  fs.delete("/startup.lua")
+  fs.delete("/dragonos")
+  os.reboot()
+else
+  error()
+end
