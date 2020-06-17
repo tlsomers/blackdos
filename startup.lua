138acf54c9804cd8e421dfcfa130b91cec72e618
BlackDragon-B <59388636+BlackDragon-B@users.noreply.github.com>
2020-06-17 08:29:26 +0200

    Fixed startup.lua

--- startup.lua
+++ startup.lua
@@ -13,9 +13,7 @@ function os.distro()
 	return distro
 end
 
--- Set alias
-shell.setAlias( "test", "dragonos/programs/test.lua" )
-shell.setAlias( "uninstall", "dragonos/programs/uninstall.lua" )
+shell.setPath(shell.path()..":/dragonos/programs")
 
 term.setTextColor(colors.yellow)
 term.setCursorPos(1,1)
