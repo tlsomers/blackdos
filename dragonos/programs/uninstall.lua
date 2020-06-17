47b1318ef17f6bb13dd6eeafa03a53ff88c75ff4
BlackDragon-B <59388636+BlackDragon-B@users.noreply.github.com>
2020-06-17 07:59:56 +0200

    Update uninstall.lua

--- dragonos/programs/uninstall.lua
+++ dragonos/programs/uninstall.lua
@@ -1,7 +1,7 @@
 -- Uninstall by BlackDragon#8528
 -- Less destructive way of removing the OS
 
-write("Are you sure u want to uninstall"..ver.."? Y/N ")
+write("Are you sure u want to uninstall"..os.ver.."? Y/N ")
 local choice = read()
 if choice == Y or choice == y then
   fs.delete("/startup.lua")
